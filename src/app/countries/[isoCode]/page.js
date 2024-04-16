ec9d0032b60d8b6ca77990dbf37ea606777f6fd0
Adam Planko <adi.planko@gmail.com>
2024-04-16 22:55:01 +0200

    all countries

--- src/app/countries/[isoCode]/page.js
+++ src/app/countries/[isoCode]/page.js
@@ -2,17 +2,21 @@ export default async function CountryPage({ params }) {
   const res = await fetch(
     "https://restcountries.com/v3.1/alpha/" + params.isoCode
   );
-  const country = await res.json();
+  const country = (await res.json())[0];
   console.log(country);
 
   return (
     <>
-      <img
-        src={`https://countryflagsapi.netlify.app/flag/${params.isoCode}.svg`}
-        className="w-64"
-      ></img>
-      <h1>{params.isoCode}</h1>
-      <p>{JSON.stringify(country)}</p>
+      <div className="flex flex-col items-center">
+        <img
+          src={`https://countryflagsapi.netlify.app/flag/${params.isoCode}.svg`}
+          className="w-64 mb-5"
+        ></img>
+        <h1 className="font-bold text-white text-4xl">{country.name.common}</h1>
+        <h1 className="italic text-zinc-300 text-lg">
+          {country.name.official}
+        </h1>
+      </div>
     </>
   );
 }
--- src/app/countries/page.js
+++ src/app/countries/page.js
@@ -1,14 +1,20 @@
 import Link from "next/link";
 
-export default function Countries(params) {
+export default async function Countries(params) {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const countries = await res.json();
+  console.log(countries);
+
+  const listItems = countries.map((c) => (
+    <li>
+      <Link href={`/countries/${c.cca2}`}>{c.name.common}</Link>
+    </li>
+  ));
+
   return (
     <div>
       <h1>CounTrees</h1>
-      <ul>
-        <li>
-          <Link href="/countries/SK">Slovakia</Link>
-        </li>
-      </ul>
+      <ul>{listItems}</ul>
     </div>
   );
 }
